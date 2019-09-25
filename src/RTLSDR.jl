e0e840f450f5894bd8a079b3207e8aa28dff4f73
dressel <louisdressel@gmail.com>
2019-09-25 06:09:42 -0700

    making tabs consistent and spaces

--- src/RTLSDR.jl
+++ src/RTLSDR.jl
@@ -12,51 +12,50 @@ export get_rate, set_rate, get_freq, set_freq
 include("c_interface.jl")
 
 mutable struct RtlSdr
-	valid_ptr::Bool
-	dongle_ptr::Ptr{rtlsdr_dev}
+    valid_ptr::Bool
+    dongle_ptr::Ptr{rtlsdr_dev}
 
-	function RtlSdr()
-		dp = rtlsdr_open()
+    function RtlSdr()
+        dp = rtlsdr_open()
 
-		r = new(true, dp)
+        r = new(true, dp)
 
-		# set sample rate and center freq
-		set_rate(r, 2.0e6)
-		set_freq(r, 88.5e6)
+        # set sample rate and center freq
+        set_rate(r, 2.0e6)
+        set_freq(r, 88.5e6)
 
-		return r
-	end
+        return r
+    end
 end
 
 function open(r::RtlSdr, device_index::Int)
 end
 
-#close(r::RtlSdr) = rtlsdr_close(r.dongle_ptr)
 function close(r::RtlSdr)
-	rtlsdr_close(r.dongle_ptr)
-	r.valid_ptr = false
+    rtlsdr_close(r.dongle_ptr)
+    r.valid_ptr = false
 end
 
 # Sample rate in MHz
 function get_rate(r::RtlSdr)
-	@assert r.valid_ptr
-	rate = rtlsdr_get_sample_rate(r.dongle_ptr)
-	return Int(rate)
+    @assert r.valid_ptr
+    rate = rtlsdr_get_sample_rate(r.dongle_ptr)
+    return Int(rate)
 end
 function set_rate(r::RtlSdr, sample_rate)
-	@assert r.valid_ptr
-	rtlsdr_set_sample_rate(r.dongle_ptr, sample_rate)
+    @assert r.valid_ptr
+    rtlsdr_set_sample_rate(r.dongle_ptr, sample_rate)
 end
 
 # center frequency in MHz
 function set_freq(r::RtlSdr, freq)
-	@assert r.valid_ptr
-	rtlsdr_set_center_freq(r.dongle_ptr, freq)
+    @assert r.valid_ptr
+    rtlsdr_set_center_freq(r.dongle_ptr, freq)
 end
 function get_freq(r::RtlSdr)
-	@assert r.valid_ptr
-	freq = rtlsdr_get_center_freq(r.dongle_ptr)
-	return Int(freq)
+    @assert r.valid_ptr
+    freq = rtlsdr_get_center_freq(r.dongle_ptr)
+    return Int(freq)
 end
 
 
@@ -66,87 +65,87 @@ end
 Returns a vector of length `num_samples` with complex numbers.
 """
 function read_samples(r::RtlSdr, num_samples)
-	@assert r.valid_ptr
-	num_bytes = 2num_samples
-	raw_data = read_bytes(r.dongle_ptr, num_bytes)
-	return packed_bytes_to_iq(raw_data)
+    @assert r.valid_ptr
+    num_bytes = 2num_samples
+    raw_data = read_bytes(r.dongle_ptr, num_bytes)
+    return packed_bytes_to_iq(raw_data)
 end
 function packed_bytes_to_iq(bytes)
-	num_bytes = length(bytes)
-	num_iq = round(Int, num_bytes/2.0, RoundDown)
-	iq_vals = zeros(Complex{Float64}, num_iq)
-	den = 255.0/2.0
+    num_bytes = length(bytes)
+    num_iq = round(Int, num_bytes/2.0, RoundDown)
+    iq_vals = zeros(Complex{Float64}, num_iq)
+    den = 255.0/2.0
 
-	for i = 1:num_iq
-		iq_vals[i] = bytes[2i-1]/den - 1.0 + im*(bytes[2i]/den - 1.0)
-	end
+    for i = 1:num_iq
+        iq_vals[i] = bytes[2i-1]/den - 1.0 + im*(bytes[2i]/den - 1.0)
+    end
 
-	return iq_vals
+    return iq_vals
 end
 
 # maximum 
 function get_strength(r::RtlSdr, n=10; plot_max::Bool=false)
-	max_sample = -Inf
-	max_p = 0
-	sample_rate = get_rate(r)
-	for i = 1:n
-		#samples = read_samples(r,256*10240)
-		#samples = read_samples(r, 256 * 1024 * 8)
-		#samples = read_samples(r,256*1024)
-		#samples = read_samples(r,256*940)
-		#samples = read_samples(r,256*920)
-		#samples = read_samples(r,256*900)
-		#samples = read_samples(r,256*800)
-		samples = read_samples(r, 256*500)
-		#samples = read_samples(r, 256*50)
-		p = welch_pgram(samples, fs=sample_rate)
-		temp_max = maximum(p.power)
-		if temp_max > max_sample 
-			max_sample = temp_max
-			max_p = deepcopy(p)
-		end
-	end
-	if plot_max
-		center_freq = get_freq(r)
-		plot(max_p.freq + center_freq, 10log10(max_p.power))
-	end
-	return 10log10(max_sample)
+    max_sample = -Inf
+    max_p = 0
+    sample_rate = get_rate(r)
+    for i = 1:n
+        #samples = read_samples(r,256*10240)
+        #samples = read_samples(r, 256 * 1024 * 8)
+        #samples = read_samples(r,256*1024)
+        #samples = read_samples(r,256*940)
+        #samples = read_samples(r,256*920)
+        #samples = read_samples(r,256*900)
+        #samples = read_samples(r,256*800)
+        samples = read_samples(r, 256*500)
+        #samples = read_samples(r, 256*50)
+        p = welch_pgram(samples, fs=sample_rate)
+        temp_max = maximum(p.power)
+        if temp_max > max_sample
+            max_sample = temp_max
+            max_p = deepcopy(p)
+        end
+    end
+    if plot_max
+        center_freq = get_freq(r)
+        plot(max_p.freq + center_freq, 10log10(max_p.power))
+    end
+    return 10log10(max_sample)
 end
 
 
 
 function get_strength2(r::RtlSdr, n=10; plot_max::Bool=false)
-	max_sample = -Inf
-	max_p = 0
-	sample_rate = get_rate(r)
-	max_freqs = 0
-	max_pows = 0
-	for i = 1:n
-		samples = read_samples(r, 256*500)
-		p = welch_pgram(samples, fs=sample_rate)
-		n_plot = round(Int, length(p.power) / 2, RoundDown)-3
-		pows = p.power[2:n_plot]
-		freqs = p.freq[2:n_plot]
+    max_sample = -Inf
+    max_p = 0
+    sample_rate = get_rate(r)
+    max_freqs = 0
+    max_pows = 0
+    for i = 1:n
+        samples = read_samples(r, 256*500)
+        p = welch_pgram(samples, fs=sample_rate)
+        n_plot = round(Int, length(p.power) / 2, RoundDown)-3
+        pows = p.power[2:n_plot]
+        freqs = p.freq[2:n_plot]
 
-		# taking maximum...
-		temp_max = maximum(pows)
+        # taking maximum...
+        temp_max = maximum(pows)
 
-		# taking spectrum max...
-		temp_max = sum(pows)
+        # taking spectrum max...
+        temp_max = sum(pows)
 
-		if temp_max > max_sample 
-			max_sample = temp_max
-			max_p = deepcopy(p)
-			max_pows = deepcopy(pows)
-			max_freqs = deepcopy(freqs)
-		end
+        if temp_max > max_sample
+            max_sample = temp_max
+            max_p = deepcopy(p)
+            max_pows = deepcopy(pows)
+            max_freqs = deepcopy(freqs)
+        end
 	end
-	if plot_max
-		center_freq = get_freq(r)
-		#plot(max_p.freq + center_freq, 10log10(max_p.power))
-		plot(max_freqs + center_freq, 10log10(max_pows))
-	end
-	return 10log10(max_sample*146.)
+    if plot_max
+        center_freq = get_freq(r)
+        #plot(max_p.freq + center_freq, 10log10(max_p.power))
+        plot(max_freqs + center_freq, 10log10(max_pows))
+    end
+    return 10log10(max_sample*146.)
 end
 
 
--- src/c_interface.jl
+++ src/c_interface.jl
@@ -12,72 +12,72 @@ end
 
 # returns a pointer to 
 function rtlsdr_open()
-	index = 0
-	rd = Array{Ptr{rtlsdr_dev},1}(undef,1)
-	ret = ccall( (:rtlsdr_open, "librtlsdr"), Cint, (Ptr{Ptr{rtlsdr_dev}}, UInt32), rd, index)
+    index = 0
+    rd = Array{Ptr{rtlsdr_dev},1}(undef,1)
+    ret = ccall( (:rtlsdr_open, "librtlsdr"), Cint, (Ptr{Ptr{rtlsdr_dev}}, UInt32), rd, index)
 
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error opening device.")); end
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error opening device.")); end
 
-	rf = rd[1]
+    rf = rd[1]
 
-	# resetting buffers is critical to reading bytes
+    # resetting buffers is critical to reading bytes
     rtlsdr_reset_buffer(rf)
 
-	return rf
+    return rf
 end
 
 function rtlsdr_reset_buffer(rf::Ref{rtlsdr_dev})
-	ret = ccall( (:rtlsdr_reset_buffer, "librtlsdr"), Cint, (Ref{rtlsdr_dev},), rf)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error resetting buffer (error code $ret).")); end
+    ret = ccall( (:rtlsdr_reset_buffer, "librtlsdr"), Cint, (Ref{rtlsdr_dev},), rf)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error resetting buffer (error code $ret).")); end
 end
 
 function rtlsdr_close(rf::Ref{rtlsdr_dev})
-	ret = ccall( (:rtlsdr_close, "librtlsdr"), Cint, (Ref{rtlsdr_dev},), rf)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error closing device (error code $ret).")); end
+    ret = ccall( (:rtlsdr_close, "librtlsdr"), Cint, (Ref{rtlsdr_dev},), rf)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error closing device (error code $ret).")); end
 end
 
 # center frequency
 function rtlsdr_set_center_freq(rf::Ref{rtlsdr_dev}, freq)
-	ret = ccall( (:rtlsdr_set_center_freq, "librtlsdr"), Cint, (Ref{rtlsdr_dev}, UInt32), rf, Cint(freq))
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting center frequency (error code $ret).")); end
+    ret = ccall( (:rtlsdr_set_center_freq, "librtlsdr"), Cint, (Ref{rtlsdr_dev}, UInt32), rf, Cint(freq))
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting center frequency (error code $ret).")); end
 end
 
 function rtlsdr_get_center_freq(rf::Ref{rtlsdr_dev})
-	ret = ccall( (:rtlsdr_get_center_freq, "librtlsdr"), UInt32, (Ref{rtlsdr_dev},), rf)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error getting center frequency (error code $ret).")); end
+    ret = ccall( (:rtlsdr_get_center_freq, "librtlsdr"), UInt32, (Ref{rtlsdr_dev},), rf)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error getting center frequency (error code $ret).")); end
 end
 
 # sample rate
 function rtlsdr_get_sample_rate(rf::Ref{rtlsdr_dev})
-	ret = ccall( (:rtlsdr_get_sample_rate, "librtlsdr"), UInt32, (Ref{rtlsdr_dev},), rf)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error getting sample rate (error code $ret).")); end
+    ret = ccall( (:rtlsdr_get_sample_rate, "librtlsdr"), UInt32, (Ref{rtlsdr_dev},), rf)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error getting sample rate (error code $ret).")); end
 end
 
 function rtlsdr_set_sample_rate(rf::Ref{rtlsdr_dev}, sample_rate)
-	ret = ccall( (:rtlsdr_set_sample_rate, "librtlsdr"), Cint, (Ref{rtlsdr_dev},UInt32), rf, sample_rate)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error satting sample rate (error code $ret).")); end
+    ret = ccall( (:rtlsdr_set_sample_rate, "librtlsdr"), Cint, (Ref{rtlsdr_dev},UInt32), rf, sample_rate)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error satting sample rate (error code $ret).")); end
 end
 
 # gain
 function rtlsdr_set_agc_mode(rf::Ref{rtlsdr_dev}, on)
-	ret = ccall( (:rtlsdr_set_tuner_gain, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Cint), rf, on)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting AGC mode (error code $ret).")); end
+    ret = ccall( (:rtlsdr_set_tuner_gain, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Cint), rf, on)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting AGC mode (error code $ret).")); end
 end
 
 function rtlsdr_set_tuner_gain_mode(rf::Ref{rtlsdr_dev}, manual)
-	ret = ccall( (:rtlsdr_set_tuner_gain_mode, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Cint), rf, manual)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting tuner gain mode (error code $ret).")); end
+    ret = ccall( (:rtlsdr_set_tuner_gain_mode, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Cint), rf, manual)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting tuner gain mode (error code $ret).")); end
 end
 
 function rtlsdr_set_tuner_gain(rf::Ref{rtlsdr_dev}, gain)
-	ret = ccall( (:rtlsdr_set_tuner_gain, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Cint), rf, gain)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting tuner gain (error code $ret).")); end
+    ret = ccall( (:rtlsdr_set_tuner_gain, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Cint), rf, gain)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error setting tuner gain (error code $ret).")); end
 end
 
 function rtlsdr_get_tuner_gain(rf::Ref{rtlsdr_dev})
-	gain = Array{Cint,1}(undef,1)
-	ret = ccall( (:rtlsdr_get_tuner_gain, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Ptr{Cint}), rf, gain)
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error getting tuner gain (error code $ret).")); end
+    gain = Array{Cint,1}(undef,1)
+    ret = ccall( (:rtlsdr_get_tuner_gain, "librtlsdr"), Cint, (Ref{rtlsdr_dev},Ptr{Cint}), rf, gain)
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error getting tuner gain (error code $ret).")); end
     return gain[1]
 end
 
@@ -85,12 +85,12 @@ end
 # Some I/O stuff
 function read_bytes(rf::Ref{rtlsdr_dev}, num_bytes)
 
-	buf = Vector{Cuchar}(undef,num_bytes)
-	bytes_read = Ref{Cint}(0)
-	#println("a was ", bytes_read[])
-	ret = ccall( (:rtlsdr_read_sync, "librtlsdr"), Int32, (Ref{rtlsdr_dev}, Ref{Cuchar}, Cint, Ref{Cint}), rf, buf, num_bytes, bytes_read)
-	#println("ret was: ", ret); println("a is ", bytes_read[])
-	if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error while reading from device (error code $ret).")); end
+    buf = Vector{Cuchar}(undef,num_bytes)
+    bytes_read = Ref{Cint}(0)
+    #println("a was ", bytes_read[])
+    ret = ccall( (:rtlsdr_read_sync, "librtlsdr"), Int32, (Ref{rtlsdr_dev}, Ref{Cuchar}, Cint, Ref{Cint}), rf, buf, num_bytes, bytes_read)
+    #println("ret was: ", ret); println("a is ", bytes_read[])
+    if ret != 0; throw(RTLSDRError("RTLSDR.jl reports: Error while reading from device (error code $ret).")); end
 
-	return buf
+    return buf
 end
